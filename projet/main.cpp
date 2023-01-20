9e135eb4225672b5e7d9667dbc41217acf8ff752
Tino <tinoloto@gmail.com>
2023-01-20 11:26:04 +0100

    modif

--- projet/main.cpp
+++ projet/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
 #include <string>
 #include <filesystem>
 
@@ -9,20 +10,53 @@ namespace fs = filesystem;
 
 int main()
 {
-    // Création des 3 répertoires
-    // 'exemples/' : imagettes manuscrites
-    // 'exemples_modeles/' : imagettes modèles
-    // 'exemples_txt/' : fichier texte de chaque imagette manuscrite
-    const string cheminImg = "test/";
-    const string cheminModele = "test_modeles/";
-    const string cheminTxt = "test_txt/";
+    for (int i = 1; i < 6; i++) {
+        // Charger l'image
+        Mat img = imread("exemples/casualty_000_10_5_" + to_string(i) + ".png");
 
-    // Chemin du dossier de donnees
-    const string donnees = "../donnees/";
+        // Créer un objet SIFT
+        Ptr<SIFT> sift = SIFT::create(50);
 
-    fs::create_directories(cheminImg);
-    fs::create_directories(cheminModele);
-    fs::create_directories(cheminTxt);
+        // Détecter et extraire les caractéristiques de l'image
+        vector<KeyPoint> keypoints;
+        Mat descriptors;
+        sift->detectAndCompute(img, noArray(), keypoints, descriptors);
+
+        // Afficher les caractéristiques extraites sur l'image
+        Mat img_keypoints;
+        drawKeypoints(img, keypoints, img_keypoints);
+        imshow("Image", img_keypoints);
+        waitKey();
+    }
+
+    /*
+
+    double density = countNonZero(img) / (double)(img.rows * img.cols);
+
+    Moments moments(img, true);
+    Point2f center(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+
+
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+    findContours(img, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+
+    Rect bounding_rect;
+    for (int i = 0; i < contours.size(); i++)
+        bounding_rect = bounding_rect + boundingRect(contours[i]);
+
+    int width = bounding_rect.width;
+    int height = bounding_rect.height;
+    */
+
+    return 0;
+}
+
+
+/*
+    const string cheminImg = "exemples/";
 
     //Boucle sur toutes les pages de tous les scripters
     for (int i = 1; i < 7; i++) {
@@ -30,19 +64,11 @@ int main()
             cout << "Scripter: " << i  << " Page: " << j << endl;
 
             // Train
-            /*
+            
             stringstream buff;
             buff << setfill('0') << setw(5) << i * 100 + j;
             String str = "../all-scans/" + buff.str() + ".png";
-            */
-
-            // Test
             
-            stringstream buff1;
-            buff1 << setfill('0') << setw(2) << i;
-            stringstream buff2;
-            buff2 << setfill('0') << setw(4) << j;
-            String str = donnees + "s" + buff1.str() + "_" + buff2.str() + ".png";
             
 
             // Appel de la fonction de détection de contours + enregistrement des fichiers .png + .txt
@@ -52,3 +78,4 @@ int main()
     waitKey();
     return 0;
 }
+*/
